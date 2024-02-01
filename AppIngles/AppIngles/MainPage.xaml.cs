a41a7a59ac82196cc1032f9f1ddde1e218453c9d
Aluno <Aluno@SNCTM202LABF09>
2024-01-31 22:19:06 -0300

    Adicionar arquivos de projeto.

---
+++ AppIngles/AppIngles/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppIngles
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCores_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new PageCores());
+        }
+
+        private void btnNumeros_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new PageNumeros());
+        }
+
+        private void btnFamilia_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new PageFamilia());
+        }
+
+        private void btnSair_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                System.Diagnostics.Process.GetCurrentProcess().Kill();
+            }
+            catch
+            {
+                
+                
+            }
+        }
+    }
+}
